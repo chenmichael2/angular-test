9e5961f26d5bd19ee4178bf0b0b853521f9741d3
Michael Chen <chenmichael2012@gmail.com>
2022-09-06 22:48:17 -0700

    adding backend fake server and interactions

--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of} from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from 'src/app/mock.tasks';
 
@@ -7,5 +9,11 @@ import { TASKS } from 'src/app/mock.tasks';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
 }
